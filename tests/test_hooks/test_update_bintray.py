7d38c9a13227c7730650bed2aeb201df1f36fd48
Uilian Ries <uilianries@gmail.com>
2019-01-10 09:43:22 -0200

    Apply context env to test bintray
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- tests/test_hooks/test_update_bintray.py
+++ tests/test_hooks/test_update_bintray.py
@@ -5,8 +5,10 @@ import re
 import textwrap
 import json
 from mock import mock
+from tests.utils.environ_vars import context_env
 
 from conans import tools
+
 from conans.client.remote_registry import Remote
 from conans.model.ref import ConanFileReference
 from conans.test.utils.tools import TestBufferConanOutput
@@ -17,18 +19,17 @@ from hooks.bintray_update import post_upload_recipe
 
 
 class MockResponse:
-    def __init__(self, json_data, status_code, ok=False, text=""):
+    def __init__(self, json_data, status_code, ok=False, text=None):
         self.json_data = json_data
         self.status_code = status_code
         self.ok = ok
-        self.text = text
+        self.text = str(json_data) if not text else text
 
     def json(self):
         return self.json_data
 
 
-def side_effect_get(*args, **kwargs):
-    url = kwargs.get("url")
+def side_effect_get(url, *args, **kwargs):
     if url:
         match = re.search(pattern='packages\/(.*)\/(.*)\/(.*)%3A(.*)', string=url)
         if not match:
@@ -38,7 +39,7 @@ def side_effect_get(*args, **kwargs):
             "repo": match.group(2),
             "owner": match.group(1),
             "desc": "Dummy package",
-            "labels": ["conan","testing","hooks"],
+            "labels": ["conan", "testing", "hooks"],
             "attribute_names": ["maturity"],
             "licenses": ["MIT"],
             "custom_licenses": [],
@@ -56,24 +57,23 @@ def side_effect_get(*args, **kwargs):
             "vcs_url": "https://github.com/conan-community/foo",
             "maturity": "Stable"
         }
-        return MockResponse(response, 200, True, text=json.dumps(response))
+        return MockResponse(response, 200, True)
 
     return MockResponse(None, 404, text="URL must be declared")
 
-def side_effect_patch(*args, **kwargs):
-    url = kwargs.get("url")
-    auth = kwargs.get("auth")
-    json_data = kwargs.get("json")
+
+def side_effect_patch(url, auth, json, *args, **kwargs):
     if auth.username != os.getenv("CONAN_USERNAME") or auth.password != os.getenv("CONAN_PASSWORD"):
-        response = {"message":"This resource requires authentication"}
-        return MockResponse(response, 401, text=json.dumps(response))
-    if not json_data["vcs_url"]:
-        response = {"message":"Please enter a valid VCS URL for your package."}
-        return MockResponse(response, 400, text=json.dumps(response))
+        response = {"message": "This resource requires authentication"}
+        return MockResponse(response, 401)
+    if not json["vcs_url"]:
+        response = {"message": "Please enter a valid VCS URL for your package."}
+        return MockResponse(response, 400)
     if url:
-        response = {"message":"success"}
-        return MockResponse(response, 200, True, text=json.dumps(response))
+        response = {"message": "success"}
+        return MockResponse(response, 200, True)
     return MockResponse(None, 404)
+
 
 class BintrayUpdateTests(ConanClientTestCase):
     conanfile_basic = textwrap.dedent("""\
@@ -99,35 +99,45 @@ class BintrayUpdateTests(ConanClientTestCase):
 
     def _get_environ(self, **kwargs):
         kwargs = super(BintrayUpdateTests, self)._get_environ(**kwargs)
-        kwargs.update({'CONAN_HOOKS': os.path.join(os.path.dirname(__file__), '..', '..', 'hooks', 'bintray-update')})
+        kwargs.update({'CONAN_USERNAME': 'chespirito', 'CONAN_PASSWORD': 'elchapulincolorado'})
         return kwargs
 
     @mock.patch('hooks.bintray_update.requests.get', side_effect=side_effect_get)
     @mock.patch('hooks.bintray_update.requests.patch', side_effect=side_effect_patch)
     def test_conanfile_basic(self, mock_get, mock_patch):
         reference = ConanFileReference.loads("foo/0.1.0@bar/testing")
-        remote = Remote(name="virtual", url="https://api.bintray.com/conan/conan-community/test-distribution", verify_ssl=True)
+        remote = Remote(
+            name="virtual",
+            url="https://api.bintray.com/conan/conan-community/test-distribution",
+            verify_ssl=True)
         tools.save('conanfile.py', content=self.conanfile_basic)
         output = TestBufferConanOutput()
 
-        post_upload_recipe(output=output, conanfile_path='conanfile.py', reference=reference, remote=remote)
+        with context_env(**self._get_environ()):
+            post_upload_recipe(
+                output=output, conanfile_path='conanfile.py', reference=reference, remote=remote)
 
-        self.assertIn("Reading package info form Bintray", output)
-        self.assertIn("Inspecting recipe info ...", output)
-        self.assertIn("Bintray is outdated. Updating Bintray package info ...", output)
-        self.assertNotIn("ERROR", output)
+            self.assertIn("Reading package info form Bintray", output)
+            self.assertIn("Inspecting recipe info ...", output)
+            self.assertIn("Bintray is outdated. Updating Bintray package info ...", output)
+            self.assertNotIn("ERROR", output)
 
     @mock.patch('hooks.bintray_update.requests.get', side_effect=side_effect_get)
     @mock.patch('hooks.bintray_update.requests.patch', side_effect=side_effect_patch)
     def test_conanfile_unfilled(self, mock_get, mock_patch):
         reference = ConanFileReference.loads("foo/0.1.0@bar/testing")
-        remote = Remote(name="virtual", url="https://api.bintray.com/conan/conan-community/test-distribution", verify_ssl=True)
+        remote = Remote(
+            name="virtual",
+            url="https://api.bintray.com/conan/conan-community/test-distribution",
+            verify_ssl=True)
         tools.save('conanfile.py', content=self.conanfile_limited)
         output = TestBufferConanOutput()
 
-        post_upload_recipe(output=output, conanfile_path='conanfile.py', reference=reference, remote=remote)
+        with context_env(**self._get_environ()):
+            post_upload_recipe(
+                output=output, conanfile_path='conanfile.py', reference=reference, remote=remote)
 
-        self.assertIn("Reading package info form Bintray", output)
-        self.assertIn("Inspecting recipe info ...", output)
-        self.assertIn("Bintray package info is up-to-date.", output)
-        self.assertNotIn("ERROR", output)
+            self.assertIn("Reading package info form Bintray", output)
+            self.assertIn("Inspecting recipe info ...", output)
+            self.assertIn("Bintray package info is up-to-date.", output)
+            self.assertNotIn("ERROR", output)
