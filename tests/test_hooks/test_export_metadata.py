d38328126e0f95383446e3a2522d1c857990169f
Javier G. Sogo <jgsogo@gmail.com>
2019-01-31 12:16:00 +0100

    need to yield, so finally is executed

--- tests/test_hooks/test_export_metadata.py
+++ tests/test_hooks/test_export_metadata.py
@@ -7,6 +7,7 @@ import sys
 import textwrap
 import unittest
 
+import six
 from parameterized import parameterized
 from six.moves.urllib.parse import quote
 
@@ -124,9 +125,9 @@ class ExportMetadataTests(ConanClientTestCase):
         # Check that the file is in the export folder, contains expected data and is in the manifest
         output = self.conan(['get', reference, METADATA_FILENAME])
         data = json.loads(output)
-        self.assertDictEqual(data, {'type': 'svn',
-                                    'url': repo_url,
-                                    'revision': svn.get_revision(),
-                                    'dirty': bool(not pristine_repo)})
+        self.assertDictEqual(data, {six.u('type'): six.u('svn'),
+                                    six.u('url'): repo_url,
+                                    six.u('revision'): six.u(svn.get_revision()),
+                                    six.u('dirty'): bool(not pristine_repo)})
         output = self.conan(['get', reference, CONAN_MANIFEST])
         self.assertIn(METADATA_FILENAME, output)
--- tests/utils/test_cases/conan_client.py
+++ tests/utils/test_cases/conan_client.py
@@ -7,9 +7,8 @@ import unittest
 import uuid
 from io import StringIO
 
-from conans.__init__ import __version__ as conan_version
-from conans.client.command import Conan, CommandOutputer, Command, SUCCESS
-from conans.model.version import Version
+from conans.client.command import SUCCESS
+from tests.utils.conan_command import conan_command
 from tests.utils.environ_vars import context_env
 
 
@@ -27,25 +26,17 @@ class ConanClientTestCase(unittest.TestCase):
 
     def conan(self, command, expected_return_code=SUCCESS):
         with context_env(**self._get_environ()):
-            # This snippet reproduces code from conans.client.command.main, we cannot directly
-            # use it because in case of error it is exiting the python interpreter :/
-            conan_api, cache, user_io = Conan.factory()
             output_stream = StringIO()
-            user_io.out._stream = output_stream
-            outputer = CommandOutputer(user_io, cache)
-            cmd = Command(conan_api, cache, user_io, outputer)
-            try:
-                return_code = cmd.run(command)
-            except Exception as e:
-                # Conan execution failed
-                self.fail("Conan execution for this test failed with {}: {}".format(type(e), e))
-            else:
-                # Check return code
-                self.assertEqual(return_code, expected_return_code,
-                                 msg="Unexpected return code\n\n{}".format(output_stream.getvalue()))
-            finally:
-                if Version(conan_version) < "1.13":
-                    conan_api._remote_manager._auth_manager._localdb.connection.close()  # Close sqlite3
+            with conan_command(output_stream) as cmd:
+                try:
+                    return_code = cmd.run(command)
+                except Exception as e:
+                    # Conan execution failed
+                    self.fail("Conan execution for this test failed with {}: {}".format(type(e), e))
+                else:
+                    # Check return code
+                    self.assertEqual(return_code, expected_return_code,
+                                     msg="Unexpected return code\n\n{}".format(output_stream.getvalue()))
             return output_stream.getvalue()
 
     def setUp(self):
