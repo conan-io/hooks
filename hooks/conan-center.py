4ed876a4020721332387d3b6db5693f97c64dcd3
a4z <harald.achitz@gmail.com>
2021-02-19 14:05:39 +0100

    Add KB-H013 zulu-openjdk exception
    Please see https://github.com/conan-io/conan-center-index/pull/4491 why
    this is required.

--- hooks/conan-center.py
+++ hooks/conan-center.py
@@ -730,7 +730,7 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
 
     @run_test("KB-H013", output)
     def test(out):
-        if conanfile.name in ["cmake", "android-ndk"]:
+        if conanfile.name in ["cmake", "android-ndk", "zulu-openjdk"]:
             return
         known_folders = ["lib", "bin", "include", "res", "licenses"]
         for filename in os.listdir(conanfile.package_folder):
@@ -748,7 +748,7 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
     def test(out):
         if conanfile.version == "system":
             return
-        
+
         # INFO: Whitelist for package names
         if conanfile.name in ["ms-gsl", "cccl", "poppler-data", "extra-cmake-modules", "gnu-config", "autoconf", "automake"]:
             return
