76e5359726232399e4d6ce08ca9cdb3f28f2ab5c
danimtb <danimanzaneque@gmail.com>
2021-06-04 17:47:25 +0200

    fix windows

--- .ci/generate_env_windows.bat
+++ .ci/generate_env_windows.bat
@@ -1,5 +1,6 @@
 set PATH=%PATH%;C:/Python36/Scripts/
 pip install tox==3.7.0 tox-venv==0.3.1 requests virtualenv
+python .ci/last_conan_version.py
 
 IF "%PYVER%"=="py38" (
     set PYVER="Python38-64"
@@ -12,7 +13,4 @@ IF "%PYVER%"=="py27" (
 )
 set TEST_FOLDER=D:/J/t/Hooks/%BUILD_NUMBER%/%PYVER%
 
-virtualenv --python "C:/%PYVER%/python.exe" %TEST_FOLDER% && %TEST_FOLDER%/Scripts/activate
-
-python --version
-python .ci/last_conan_version.py
+virtualenv --python "C:/%PYVER%/python.exe" %TEST_FOLDER% && %TEST_FOLDER%/Scripts/activate && python --version
