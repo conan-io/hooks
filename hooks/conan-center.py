5ac6c12edad4f258dab4fbb37cd4be3ea5bffa58
Uilian Ries <uilianries@gmail.com>
2021-07-02 06:07:54 -0300

    KB-H060 Read only gitattribute (#317)
    * KB-H060 Read only gitattribute
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * KB-H060 Read only gitattribute
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * KB-H060 Allow extra files
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * KB-H060 Check Qt files
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * KB-H060 Check Qt files
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Update hooks/conan-center.py
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    
    * Update hooks/conan-center.py
    
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>
    
    * KB-H060 Inspect assembly files
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    Co-authored-by: SpaceIm <30052553+SpaceIm@users.noreply.github.com>
    Co-authored-by: Anonymous Maarten <madebr@users.noreply.github.com>

--- hooks/conan-center.py
+++ hooks/conan-center.py
@@ -634,11 +634,18 @@ def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
 
     @run_test("KB-H060", output)
     def test(out):
+        ext_to_be_checked = [".cmake", ".conf", ".cfg", ".diff", ".md", ".patch", ".py", ".txt",
+                             ".yml", ".am", ".xml", ".json", ".in", ".ac", ".tsx", ".tmx",
+                             ".proto", ".capnp", ".c", ".cc", ".c++", ".cpp", ".cxx", ".c++m",
+                             ".cppm", ".cxxm", ".h++", ".hh", ".hxx", ".hpp", ".qrc", ".pro",
+                             ".build", ".s", ".asm"]
         recipe_folder = os.path.dirname(conanfile_path)
         for root, _, files in os.walk(recipe_folder):
             if os.path.relpath(root, recipe_folder).replace("\\", "/").startswith("test_package/build"):
                 continue
             for filename in files:
+                if not any(filename.lower().endswith(ext) for ext in ext_to_be_checked):
+                    continue
                 lines = open(os.path.join(root, filename), 'rb').readlines()
                 if any(line.endswith(b'\r\n') for line in lines):
                     out.error("The file '{}' uses CRLF. Please, replace by LF."
