ee4dd5f49f56d9d970d2fb25421776b974f5103b
Javier G. Sogo <jgsogo@gmail.com>
2021-07-23 09:33:30 +0200

    KB014 - add exception for xorg-macros (#321)

--- hooks/conan-center.py
+++ hooks/conan-center.py
@@ -821,7 +821,8 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
             return
 
         # INFO: Whitelist for package names
-        if conanfile.name in ["ms-gsl", "cccl", "poppler-data", "extra-cmake-modules", "gnu-config", "autoconf", "automake"]:
+        if conanfile.name in ["ms-gsl", "cccl", "poppler-data", "extra-cmake-modules", "gnu-config",
+                              "autoconf", "automake", "xorg-macros"]:
             return
         if not _files_match_settings(conanfile, conanfile.package_folder, out):
             out.error("Packaged artifacts does not match the settings used: os=%s, compiler=%s"
