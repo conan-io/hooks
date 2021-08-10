9da112a93ae6e71133da560a86421a0d376edfb7
Sparik Hayrapetyan <sparikhayrapetyan@gmail.com>
2021-08-10 14:43:31 +0400

    add 'autoconf-archive' to the whitelist of package names for the KB-H014 hook (#329)

--- hooks/conan-center.py
+++ hooks/conan-center.py
@@ -838,7 +838,7 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
 
         # INFO: Whitelist for package names
         if conanfile.name in ["ms-gsl", "cccl", "poppler-data", "extra-cmake-modules", "gnu-config",
-                              "autoconf", "automake", "xorg-macros"]:
+                              "autoconf", "automake", "xorg-macros", "autoconf-archive"]:
             return
         if not _files_match_settings(conanfile, conanfile.package_folder, out):
             out.error("Packaged artifacts does not match the settings used: os=%s, compiler=%s"
