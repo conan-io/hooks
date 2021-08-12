8c9a84fed7ab8cc6b8bcdf37f8927227b30e3da1
Uilian Ries <uilianries@gmail.com>
2021-08-12 11:57:26 -0300

    Add invalid topics
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ tests/test_hooks/conan-center/test_invalid_topics.py
@@ -0,0 +1,37 @@
+import os
+import textwrap
+
+from conans import tools
+
+from tests.utils.test_cases.conan_client import ConanClientTestCase
+
+
+class TestInvalidTopics(ConanClientTestCase):
+    conanfile = textwrap.dedent("""\
+        from conans import ConanFile
+        class AConan(ConanFile):
+            topics = ("foobar",)
+            pass
+        """)
+
+    def _get_environ(self, **kwargs):
+        kwargs = super(TestInvalidTopics, self)._get_environ(**kwargs)
+        kwargs.update({'CONAN_HOOKS': os.path.join(os.path.dirname(__file__), '..', '..', '..',
+                                                   'hooks', 'conan-center')})
+        return kwargs
+
+    def test_valid_topics(self):
+        tools.save('conanfile.py', content=self.conanfile)
+        output = self.conan(['export', '.', 'name/version@user/channel'])
+        self.assertIn("[INVALID TOPICS (KB-H064)] OK", output)
+
+    def test_no_topics(self):
+        tools.save('conanfile.py', content=self.conanfile.replace('topics = ("foobar",)', ""))
+        output = self.conan(['export', '.', 'name/version@user/channel'])
+        self.assertIn("[INVALID TOPICS (KB-H064)] OK", output)
+
+    def test_invalid_topics(self):
+        tools.save('conanfile.py', content=self.conanfile.replace('"foobar",', '"foobar", "conan"'))
+        output = self.conan(['export', '.', 'name/version@user/channel'])
+        self.assertIn("WARN: [INVALID TOPICS (KB-H064)] The topic 'conan' is invalid and should be"
+                      " removed from topics attribute.", output)
