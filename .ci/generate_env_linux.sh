6a618bfa0d810728befeed28e039288a83b572c2
jgsogo <jgsogo@gmail.com>
2021-10-01 18:15:12 +0200

    but probably it hasn't been regenerated for ages... https://github.com/conan-io/conan_ci_jenkins/blob/master/resources/org/jfrog/conanci/python_runner/conf.py\#L19

--- .ci/generate_env_linux.sh
+++ .ci/generate_env_linux.sh
@@ -11,20 +11,20 @@ case "${PYVER}" in
         pyenv virtualenv 2.7.18 conan
         ;;
     py36)
-        pyenv install 3.6.15
-        pyenv virtualenv 3.6.15 conan
+        pyenv install 3.6.12
+        pyenv virtualenv 3.6.12 conan
         ;;
     py37)
         pyenv install 3.7.12
         pyenv virtualenv 3.7.12 conan
         ;;
     py38)
-        pyenv install 3.8.12
-        pyenv virtualenv 3.8.12 conan
+        pyenv install 3.8.6
+        pyenv virtualenv 3.8.6 conan
         ;;
     py39)
-        pyenv install 3.9.7
-        pyenv virtualenv 3.9.7 conan
+        pyenv install 3.9.2
+        pyenv virtualenv 3.9.2 conan
         ;;
 esac
 
