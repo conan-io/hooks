1fc7d172df6366877b399f06bde8d00577224780
jgsogo <jgsogo@gmail.com>
2021-10-04 13:17:30 +0200

    Skip emsdk from KB-H021

--- hooks/conan-center.py
+++ hooks/conan-center.py
@@ -994,7 +994,7 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
 
     @run_test("KB-H021", output)
     def test(out):
-        if conanfile.name in ["powershell", "android-ndk"]:
+        if conanfile.name in ["powershell", "android-ndk", "emsdk"]:
             return
         bad_files = _get_files_following_patterns(conanfile.package_folder,
                                                   ["msvcr*.dll", "msvcp*.dll", "vcruntime*.dll", "concrt*.dll"])
