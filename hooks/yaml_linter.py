3cfc6cdddae99f63e912e197fc272ddbefe5b8bf
ericLemanissier <ericLemanissier@users.noreply.github.com>
2021-10-06 20:25:56 +0200

    Update yaml_linter.py

--- hooks/yaml_linter.py
+++ hooks/yaml_linter.py
@@ -14,15 +14,15 @@ CONAN_HOOK_YAMLLINT_WERR = "CONAN_YAMLLINT_WERR"
 def pre_export(output, conanfile_path, *args, **kwargs):
     output.info("Lint yaml '{}'".format(conanfile_path))
     conanfile_dirname = os.path.dirname(conanfile_path)
-    conandata_file = os.path.join(conanfile_dirname, 'conadata.yaml')
+    conandata_file = os.path.join(conanfile_dirname, 'conandata.yaml')
     
     rules = {
         "document-start": "disable",
         "line-length": "disable",
-        "new-lines": "level: warning",
-        "empty-lines": "level: warning",
-        "indentation": "level: warning"
-        "trailing-spaces": "level: warning",
+        "new-lines": "{level: warning}",
+        "empty-lines": "{level: warning}",
+        "indentation": "{level: warning}",
+        "trailing-spaces": "{level: warning}",
     }
 
     lint_args = ['-f', 'parsable',
@@ -30,7 +30,7 @@ def pre_export(output, conanfile_path, *args, **kwargs):
                  ", ".join("%s: %s" % (r, rules[r]) for r in rules)]
 
     try:
-        command = ['yamllint'] + lint_args + [conandata_file.replace('\\', '/')]
+        command = ['yamllint'] + lint_args + ['"%s"' % conandata_file.replace('\\', '/')]
         command = " ".join(command)
         shell = bool(platform.system() != "Windows")
         p = subprocess.Popen(command, shell=shell, bufsize=10,
@@ -41,8 +41,12 @@ def pre_export(output, conanfile_path, *args, **kwargs):
         output.error("Unexpected error running linter: {}".format(exc))
         return
     errors = 0
-    for line in messages.splitlines():
+    for line in yamllint_stdout.splitlines():
         output.info(line)
+        i = line.find(":")
+        line = line[i:]
+        i = line.find(":")
+        line = line[i:]
         parts = line.split(' ')
         errors += int(parts[1] == "[error]")
 
