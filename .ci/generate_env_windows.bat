ba1da52b3ee392972623dc1d363c6a1a1de10184
Uilian Ries <uilianries@gmail.com>
2022-05-03 13:13:08 +0200

    change test folder path
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/generate_env_windows.bat
+++ .ci/generate_env_windows.bat
@@ -2,6 +2,8 @@ set PATH=%PATH%;C:/Python36/Scripts/
 pip install --timeout 100 --retries 10 tox==3.7.0 tox-venv==0.3.1 requests virtualenv
 python .ci/last_conan_version.py
 
+set TEST_FOLDER=D:/J/t/Hooks/%BUILD_NUMBER%/%PYVER%
+
 IF "%PYVER%"=="py39" (
     set PYVER="Python39"
 )
@@ -11,7 +13,7 @@ IF "%PYVER%"=="py38" (
 IF "%PYVER%"=="py36" (
     set PYVER="Python36"
 )
-set TEST_FOLDER=D:/J/t/Hooks/%BUILD_NUMBER%/%PYVER%
+
 
 virtualenv --python "C:/%PYVER%/python.exe" %TEST_FOLDER% && %TEST_FOLDER%/Scripts/activate && python --version
 python -m pip install pip --upgrade
