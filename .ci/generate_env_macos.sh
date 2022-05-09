e800dfbb593eb74e2f9f5b18a7ab67334221cb9d
Uilian Ries <uilianries@gmail.com>
2022-05-09 12:18:03 +0200

    Support 3.10
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/generate_env_macos.sh
+++ .ci/generate_env_macos.sh
@@ -6,12 +6,14 @@ set -x
 # https://github.com/conan-io/conan_ci_jenkins/blob/master/resources/org/jfrog/conanci/python_runner/conf.py
 TEST_FOLDER="${TMPDIR}/${PYVER}"
 VENV_FOLDER="${TEST_FOLDER}/venv"
-PYENV_ROOT=${TMPDIR}
-PYENV="${TMPDIR}/.pyenv/bin/pyenv"
+PYENV_ROOT=${TEST_FOLDER}
+PYENV="${TEST_FOLDER}/.pyenv/bin/pyenv"
 
 mkdir -p ${TEST_FOLDER} || echo "ok"
 
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+curl -s -L -o "${TEST_FOLDER}/pyenv-installer" https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer
+chmod +x "${TEST_FOLDER}/pyenv-installer"
+PYENV_ROOT=${PYENV_ROOT} "${TEST_FOLDER}/pyenv-installer"
 eval "$(${PYENV} init -)"
 
 case "${PYVER}" in
@@ -30,6 +32,10 @@ case "${PYVER}" in
     py39)
         ${PYENV} install 3.9.2
         ${PYENV} virtualenv 3.9.2 ${VENV_FOLDER}
+        ;;
+    py310)
+        ${PYENV} install 3.10.4
+        ${PYENV} virtualenv 3.10.4 ${VENV_FOLDER}
         ;;
 esac
 
