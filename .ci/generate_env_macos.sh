bde35f4ac4ce0ee7b339215d52b67e868a69ac48
Uilian Ries <uilianries@gmail.com>
2022-05-09 12:08:25 +0200

    Jenkins pyenv
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/generate_env_macos.sh
+++ .ci/generate_env_macos.sh
@@ -6,33 +6,35 @@ set -x
 # https://github.com/conan-io/conan_ci_jenkins/blob/master/resources/org/jfrog/conanci/python_runner/conf.py
 TEST_FOLDER="${TMPDIR}/${PYVER}"
 VENV_FOLDER="${TEST_FOLDER}/venv"
+PYENV=/Users/jenkins/.pyenv/bin/pyenv
 
 mkdir -p ${TEST_FOLDER} || echo "ok"
 
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-eval "$(pyenv init -)"
+eval "$(${PYENV} init -)"
 
 case "${PYVER}" in
     py36)
-        pyenv install 3.6.12
-        pyenv virtualenv 3.6.12 ${VENV_FOLDER}
+        ${PYENV} install 3.6.12
+        ${PYENV} virtualenv 3.6.12 ${VENV_FOLDER}
         ;;
     py37)
-        pyenv install 3.7.12
-        pyenv virtualenv 3.7.12 ${VENV_FOLDER}
+        ${PYENV} install 3.7.12
+        ${PYENV} virtualenv 3.7.12 ${VENV_FOLDER}
         ;;
     py38)
-        pyenv install 3.8.12
-        pyenv virtualenv 3.8.12 ${VENV_FOLDER}
+        ${PYENV} install 3.8.12
+        ${PYENV} virtualenv 3.8.12 ${VENV_FOLDER}
         ;;
     py39)
-        pyenv install 3.9.2
-        pyenv virtualenv 3.9.2 ${VENV_FOLDER}
+        ${PYENV} install 3.9.2
+        ${PYENV} virtualenv 3.9.2 ${VENV_FOLDER}
         ;;
 esac
 
-pyenv activate ${VENV_FOLDER}
+${PYENV} activate ${VENV_FOLDER}
 python --version
+pip --version
 pip install --upgrade pip
-pip3 install --requirement .ci/requirements_macos.txt
+pip install --requirement .ci/requirements_macos.txt
 python .ci/last_conan_version.py
