57fe0549f39eb5b1364aa275be43402e508fdafd
Uilian Ries <uilianries@gmail.com>
2022-05-09 13:24:47 +0200

    Use python 3.8
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/generate_env_macos.sh
+++ .ci/generate_env_macos.sh
@@ -4,49 +4,28 @@ set -e
 set -x
 
 # https://github.com/conan-io/conan_ci_jenkins/blob/master/resources/org/jfrog/conanci/python_runner/conf.py
-TEMP_FOLDER=`mktemp -d`
-TEST_FOLDER="${TEMP_FOLDER}/${PYVER}"
-VENV_FOLDER="${TEST_FOLDER}/venv"
-PYENV_ROOT="${TEMP_FOLDER}/pyenv"
-PYENV="${PYENV_ROOT}/bin/pyenv"
-export PATH="${PYENV_ROOT}/bin:$PATH"
-
-mkdir -p ${TEST_FOLDER} || echo "ok"
-
-ls -R /Users/jenkins/.pyenv
-
-export PYENV_ROOT=${PYENV_ROOT}
-
-curl -s -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-eval "$(pyenv init --path)"
-eval "$(pyenv virtualenv-init -)"
+TEST_FOLDER="${TMPDIR}/${PYVER}"
 
 case "${PYVER}" in
     py36)
-        pyenv install 3.6.12
-        pyenv virtualenv 3.6.12 ${VENV_FOLDER}
+        PYVER="/Users/jenkins/.pyenv/versions/3.6.13/bin/python"
         ;;
     py37)
-        pyenv install 3.7.12
-        pyenv virtualenv 3.7.12 ${VENV_FOLDER}
+        PYVER="/Users/jenkins/.pyenv/versions/3.7.6/bin/python"
         ;;
     py38)
-        pyenv install 3.8.12
-        pyenv virtualenv 3.8.12 ${VENV_FOLDER}
+        PYVER="/Users/jenkins/.pyenv/versions/3.8.6/bin/python"
         ;;
     py39)
-        pyenv install 3.9.2
-        pyenv virtualenv 3.9.2 ${VENV_FOLDER}
-        ;;
-    py310)
-        pyenv install 3.10.4
-        pyenv virtualenv 3.10.4 ${VENV_FOLDER}
+        PYVER="/Users/jenkins/.pyenv/versions/3.9.0/bin/python"
         ;;
 esac
 
-pyenv activate "${VENV_FOLDER}"
-python --version
-pip --version
-pip install --upgrade pip
-pip install --requirement .ci/requirements_macos.txt
-python .ci/last_conan_version.py
+mkdir -p ${TEST_FOLDER} || echo "ok"
+${PYVER} -m pip install tox==3.7.0 tox-venv==0.3.1 requests
+${PYVER} -m venv ${TEST_FOLDER} && \
+  source ${TEST_FOLDER}/bin/activate && \
+  python --version && \
+  python -m pip install --upgrade pip && \
+  python -m pip install --upgrade --requirement .ci/requirements_macos.txt && \
+  python .ci/last_conan_version.py
