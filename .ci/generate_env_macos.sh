734e68bdb43598ef2b7cd0b2ca07d03633d2d70c
Uilian Ries <uilianries@gmail.com>
2022-05-09 12:43:18 +0200

    Update env vars
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/generate_env_macos.sh
+++ .ci/generate_env_macos.sh
@@ -4,41 +4,44 @@ set -e
 set -x
 
 # https://github.com/conan-io/conan_ci_jenkins/blob/master/resources/org/jfrog/conanci/python_runner/conf.py
-TEST_FOLDER="${TMPDIR}/${PYVER}"
+TEMP_FOLDER=`mktemp -d`
+TEST_FOLDER="${TEMP_FOLDER}/${PYVER}"
 VENV_FOLDER="${TEST_FOLDER}/venv"
-PYENV_ROOT="${TEST_FOLDER}/pyenv"
-PYENV="${PYENV_ROOT}/.pyenv/bin/pyenv"
+PYENV_ROOT="${TEMP_FOLDER}/pyenv"
+PYENV="${PYENV_ROOT}/bin/pyenv"
+export PATH="${PYENV_ROOT}/bin:$PATH"
 
 mkdir -p ${TEST_FOLDER} || echo "ok"
 export PYENV_ROOT=${PYENV_ROOT}
 
 curl -s -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-eval "$(${PYENV} init -)"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
 
 case "${PYVER}" in
     py36)
-        ${PYENV} install 3.6.12
-        ${PYENV} virtualenv 3.6.12 ${VENV_FOLDER}
+        pyenv install 3.6.12
+        pyenv virtualenv 3.6.12 ${VENV_FOLDER}
         ;;
     py37)
-        ${PYENV} install 3.7.12
-        ${PYENV} virtualenv 3.7.12 ${VENV_FOLDER}
+        pyenv install 3.7.12
+        pyenv virtualenv 3.7.12 ${VENV_FOLDER}
         ;;
     py38)
-        ${PYENV} install 3.8.12
-        ${PYENV} virtualenv 3.8.12 ${VENV_FOLDER}
+        pyenv install 3.8.12
+        pyenv virtualenv 3.8.12 ${VENV_FOLDER}
         ;;
     py39)
-        ${PYENV} install 3.9.2
-        ${PYENV} virtualenv 3.9.2 ${VENV_FOLDER}
+        pyenv install 3.9.2
+        pyenv virtualenv 3.9.2 ${VENV_FOLDER}
         ;;
     py310)
-        ${PYENV} install 3.10.4
-        ${PYENV} virtualenv 3.10.4 ${VENV_FOLDER}
+        pyenv install 3.10.4
+        pyenv virtualenv 3.10.4 ${VENV_FOLDER}
         ;;
 esac
 
-${PYENV} activate ${VENV_FOLDER}
+pyenv activate "${VENV_FOLDER}"
 python --version
 pip --version
 pip install --upgrade pip
