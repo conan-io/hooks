f812edd94384b1dadc0628833d74d8da292a8a31
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-05-18 13:48:43 +0200

    Update generate_env_macos.sh

--- .ci/generate_env_macos.sh
+++ .ci/generate_env_macos.sh
@@ -14,7 +14,7 @@ case "${PYVER}" in
         PYVER="/Users/jenkins/.pyenv/versions/3.7.6/bin/python"
         ;;
     py38)
-        PYVER="/Users/jenkins/.pyenv/versions/3.8.12/bin/python"
+        PYVER="/Users/jenkins/.pyenv/versions/3.8.13/bin/python"
         ;;
     py39)
         PYVER="/Users/jenkins/.pyenv/versions/3.9.0/bin/python"
@@ -26,7 +26,6 @@ ${PYVER} -m pip install tox==3.7.0 tox-venv==0.3.1 requests
 ${PYVER} -m venv ${TEST_FOLDER} && \
   source ${TEST_FOLDER}/bin/activate && \
   python --version && \
-  python -m pip install --upgrade "platformdirs<=2.5.1" && \
   python -m pip install --upgrade pip && \
   python -m pip install --upgrade --requirement .ci/requirements_macos.txt && \
   python .ci/last_conan_version.py
