c91cde809828d978eb9efd3a16afaec7703eb4b2
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-05-18 19:29:35 +0200

    try with clean environment

--- .ci/run_tox.sh
+++ .ci/run_tox.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # https://github.com/conan-io/conan_ci_jenkins/blob/master/resources/org/jfrog/conanci/python_runner/conf.py
-TEST_FOLDER=${TMPDIR}/${PYVER}
+TEST_FOLDER=${TMPDIR}/${BUILD_NUMBER}/${PYVER}
 
 case "${PYVER}" in
     py36)
