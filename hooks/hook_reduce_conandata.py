0bb78be1885707529fe87eb3968e06fbe44d75dc
Javier G. Sogo <jgsogo@gmail.com>
2022-08-04 12:06:08 +0200

    [conan-v2] hooks arguments changed in beta1 (#436)
    * [conan-v2] hooks arguments changed in beta1
    
    * out of quarantine

--- hooks/hook_reduce_conandata.py
+++ hooks/hook_reduce_conandata.py
@@ -10,10 +10,8 @@ def load_yml(conanfile, path):
     return None
 
 
-def post_export(output, conanfile, conanfile_path, reference, **kwargs):
-    export_folder_path = os.path.dirname(conanfile_path)
-
-    conandata_path = os.path.join(export_folder_path, "conandata.yml")
+def post_export(conanfile):
+    conandata_path = os.path.join(conanfile.export_folder, "conandata.yml")
     version = str(conanfile.version)
 
     conandata_yml = load_yml(conanfile, conandata_path)
@@ -25,7 +23,7 @@ def post_export(output, conanfile, conanfile_path, reference, **kwargs):
             continue
         info[entry] = {}
         info[entry][version] = conandata_yml[entry][version]
-    output.info("Saving conandata.yml: {}".format(info))
+    conanfile.output.info("Saving conandata.yml: {}".format(info))
     new_conandata_yml = yaml.safe_dump(info, default_flow_style=False)
-    output.info("New conandata.yml contents: {}".format(new_conandata_yml))
+    conanfile.output.info("New conandata.yml contents: {}".format(new_conandata_yml))
     save(conanfile, conandata_path, new_conandata_yml)
