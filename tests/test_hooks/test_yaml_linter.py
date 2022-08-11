e87b9948374c80776052b468aea652f9aca25708
jgsogo <jgsogo@gmail.com>
2022-08-11 12:01:03 +0200

    yaml_linter - checksum cannot be empty

--- tests/test_hooks/test_yaml_linter.py
+++ tests/test_hooks/test_yaml_linter.py
@@ -72,3 +72,21 @@ class YAMLLinterTests(ConanClientTestCase):
         recipe_path = os.path.join(os.getcwd(), "path spaces", "conanfile.py")
         self.assertIn("pre_export(): Lint yaml '{}'".format(recipe_path), output)
         self.assertIn("pre_export(): YAML Linter detected '0' errors", output)
+
+    def test_empty_checksum(self):
+        conandatafile = textwrap.dedent(r"""
+            sources:
+            "version":
+                url: "https://url.to/name/version.tar.xz"
+                sha256: ""
+            patches:
+            "version":
+                - patch_file: "patches/abcdef.diff"
+                  base_path: "source"
+            """)
+        tools.save(os.path.join("path spaces", "conanfile.py"), content=self.conanfile)
+        tools.save(os.path.join("path spaces", "conandata.py"), content=conandatafile)
+        output = self.conan(['export', 'path spaces/conanfile.py', 'name/version@'])
+        recipe_path = os.path.join(os.getcwd(), "path spaces", "conanfile.py")
+        self.assertIn("pre_export(): Lint yaml '{}'".format(recipe_path), output)
+        self.assertIn("pre_export(): YAML Linter detected '1' errors", output)
