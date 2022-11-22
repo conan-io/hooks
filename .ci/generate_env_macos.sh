e3d247a7c90bc92fd6467767d7497c3dcbc84378
Uilian Ries <uilianries@gmail.com>
2022-11-22 17:20:52 +0100

    Update mac environment according to M1 machine
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/generate_env_macos.sh
+++ .ci/generate_env_macos.sh
@@ -8,13 +8,10 @@ TEST_FOLDER="${WORKSPACE}/${PYVER}"
 
 case "${PYVER}" in
     py36)
-        PYVER="/Users/jenkins/.pyenv/versions/3.6.13/bin/python"
-        ;;
-    py37)
-        PYVER="/Users/jenkins/.pyenv/versions/3.7.6/bin/python"
+        PYVER="/Users/jenkins/.pyenv/versions/3.6.15/bin/python"
         ;;
     py38)
-        PYVER="/Users/jenkins/.pyenv/versions/3.8.12/bin/python"
+        PYVER="/Users/jenkins/.pyenv/versions/3.8.6/bin/python"
         ;;
     py39)
         PYVER="/Users/jenkins/.pyenv/versions/3.9.0/bin/python"
