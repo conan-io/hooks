163671ec8c90d8df734e72c1b668c44535da8b68
Uilian Ries <uilianries@gmail.com>
2022-11-23 13:37:47 +0100

    minimize test
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- tests/test_hooks/conan-center/test_header_only_extensions.py
+++ tests/test_hooks/conan-center/test_header_only_extensions.py
@@ -14,12 +14,9 @@ class TestHeaderOnlyExtensions(ConanClientTestCase):
 
     conanfile = textwrap.dedent("""\
         from conan import ConanFile
-        from conan.tools.files import get, copy
-        from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+        from conan.tools.cmake import cmake_layout
         from conan.tools.layout import basic_layout
         import os
-        
-        required_conan_version = ">=1.53.0"
         
         class MiniaudioConan(ConanFile):
             name = "miniaudio"
@@ -35,16 +32,6 @@ class TestHeaderOnlyExtensions(ConanClientTestCase):
                 "header_only": True,
             }
         
-            def config_options(self):
-                if self.settings.os == "Windows":
-                    del self.options.fPIC
-        
-            def configure(self):
-                if self.options.shared:
-                    self.options.rm_safe("fPIC")
-                self.settings.rm_safe("compiler.libcxx")
-                self.settings.rm_safe("compiler.cppstd")
-        
             def layout(self):
                 if self.options.header_only:
                     basic_layout(self, src_folder="src")
@@ -55,56 +42,10 @@ class TestHeaderOnlyExtensions(ConanClientTestCase):
                 if self.options.header_only:
                     self.info.clear()
         
-            def generate(self):
-                if self.options.header_only:
-                    return
-        
-                tc = CMakeToolchain(self)
-                tc.variables["MINIAUDIO_VERSION_STRING"] = self.version
-                tc.generate()
-
-            def build(self):
-                if self.options.header_only:
-                    return
-        
-                cmake = CMake(self)
-                cmake.configure(build_script_folder=os.path.join(self.source_folder, os.pardir))
-                cmake.build()
-        
-            def package(self):
-                copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
-                copy(
-                    self,
-                    pattern="**",
-                    dst=os.path.join(self.package_folder, "include", "extras"),
-                    src=os.path.join(self.source_folder, "extras"),
-                )
-        
-                if self.options.header_only:
-                    copy(
-                        self,
-                        pattern="miniaudio.h",
-                        dst=os.path.join(self.package_folder, "include"),
-                        src=self.source_folder)
-                    copy(
-                        self,
-                        pattern="miniaudio.*",
-                        dst=os.path.join(self.package_folder, "include", "extras", "miniaudio_split"),
-                        src=os.path.join(self.source_folder, "extras", "miniaudio_split"),
-                    )
-                else:
-                    cmake = CMake(self)
-                    cmake.install()
-        
             def package_info(self):
                 if self.options.header_only:
                     self.cpp_info.bindirs = []
                     self.cpp_info.libdirs = []
-                else:
-                    self.cpp_info.libs = ["miniaudio"]
-                    if self.options.shared:
-                        self.cpp_info.defines.append("MA_DLL")
-
         """)
 
     def _get_environ(self, **kwargs):
