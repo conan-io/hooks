87ce720a73a608bc2f10471767edbdb4de87ed08
danimtb <danimanzaneque@gmail.com>
2024-01-26 14:22:38 +0100

    fix requirements

--- .ci/generate_env_macos.sh
+++ .ci/generate_env_macos.sh
@@ -23,6 +23,7 @@ ${PYVER} -m venv ${TEST_FOLDER} && \
   source ${TEST_FOLDER}/bin/activate && \
   python --version && \
   python -m pip install --upgrade pip && \
+  python -m pip install -r tests/requirements_test.txt
   python -m pip install --upgrade --requirement .ci/requirements_macos.txt
   python -m pip install conan==${CONAN_VERSION}
 #  python .ci/last_conan_version.py
