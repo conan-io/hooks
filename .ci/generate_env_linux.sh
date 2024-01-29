6223a14c4cf56b69b31906b77ff5dd4abbbddd0d
danimtb <danimanzaneque@gmail.com>
2024-01-29 11:04:18 +0100

    add virtualenv name for linux

--- .ci/generate_env_linux.sh
+++ .ci/generate_env_linux.sh
@@ -8,19 +8,19 @@ eval "$(pyenv init -)"
 case "${PYVER}" in
     py36)
         pyenv install 3.6.13 || true  # Do not fail if already installed
-        pyenv virtualenv 3.6.13 conan
+        pyenv virtualenv 3.6.13 ${VIRTUALENV_NAME}
         ;;
     py38)
         pyenv install 3.8.6 || true  # Do not fail if already installed
-        pyenv virtualenv 3.8.6 conan
+        pyenv virtualenv 3.8.6 ${VIRTUALENV_NAME}
         ;;
     py39)
         pyenv install 3.9.2 || true  # Do not fail if already installed
-        pyenv virtualenv 3.9.2 conan
+        pyenv virtualenv 3.9.2 ${VIRTUALENV_NAME}
         ;;
 esac
 
-pyenv activate conan
+pyenv activate ${VIRTUALENV_NAME}
 python --version
 pip install --upgrade pip
 pip3 install --requirement tests/requirements_test.txt
