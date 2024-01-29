cbc5f438f025484a0c87514d4168b8f4854c0b7a
danimtb <danimanzaneque@gmail.com>
2024-01-29 12:20:03 +0100

    fix windows

--- .ci/run_tests.bat
+++ .ci/run_tests.bat
@@ -1,5 +1,4 @@
 REM https://github.com/conan-io/conan_ci_jenkins/blob/master/resources/org/jfrog/conanci/python_runner/conf.py
 set TEST_FOLDER=D:/J/t/Hooks/%BUILD_NUMBER%/%PYVER%
 
-%TEST_FOLDER%/Scripts/activate
-python -m pytest %PYTEST_ARGS%
+call %TEST_FOLDER%/Scripts/activate && python -m pytest %PYTEST_ARGS%
