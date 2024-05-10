a0c6fed2e5937446c97a215e78809faa8e436ff6
ericLemanissier <ericLemanissier@users.noreply.github.com>
2024-05-10 08:59:46 +0200

    KB-013: put qt in the allowlist
    allows progress on https://github.com/conan-io/conan-center-index/issues/23660

--- hooks/conan-center.py
+++ hooks/conan-center.py
@@ -1106,7 +1106,7 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
     def test(out):
         if conanfile.name in ["cmake", "android-ndk", "zulu-openjdk", "mingw-w64", "mingw-builds",
                               "openjdk", "mono", "gcc", "mold", "tz", "gawk", "maven", "binutils",
-                              "gfortran"]:
+                              "gfortran", "qt"]:
             return
 
         base_known_folders = ["lib", "bin", "include", "res", "licenses"]
