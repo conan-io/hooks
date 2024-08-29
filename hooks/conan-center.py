35a495f458fc7ed84648f721e1513f6aab262ed1
Ernesto de Gracia Herranz <ernestodegraciah@gmail.com>
2024-08-29 18:25:44 +0200

    Add allowed_list in H039 (#546)
    Co-authored-by: Abril Rincón Blanco <git@rinconblanco.es>

--- hooks/conan-center.py
+++ hooks/conan-center.py
@@ -491,8 +491,11 @@ def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
 
     @run_test("KB-H039", output)
     def test(out):
+        allowed_list = {"openjdk"}
         search_attrs = ["scm", "build_policy"]
         forbidden_attrs = []
+        if conanfile.name in allowed_list:
+            return
         for attr in search_attrs:
             if getattr(conanfile, attr, None):
                 forbidden_attrs.append(attr)
